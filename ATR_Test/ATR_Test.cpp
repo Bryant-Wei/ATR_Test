8f73fe782ea5687168d1fb5e7347361a5899df7b
Bryant-Wei <69064106+Bryant-Wei@users.noreply.github.com>
2021-03-20 10:04:57 +0800

    读取文件

---
+++ ATR_Test/ATR_Test.cpp
@@ -0,0 +1,33 @@
+﻿// ATR_Test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE 1024					//每行最大字节数
+
+int main()
+{
+
+	FILE* fp;
+	int count = 1;
+	char strLine[MAX_LINE];	//读取缓冲区
+	if ((fp = fopen("EVALUATION(1).txt", "r")) == NULL)	// 判断文件是否存在及可读
+	{
+		printf("Open Falied!");
+		return -1;
+	}
+
+	while (!feof(fp))	//循环读取每一行，直到文件尾
+	{
+		fgets(strLine, MAX_LINE, fp); // 将fp所指向的文件一行内容读到strLine缓冲区
+		printf("%s", strLine);
+		int len = strlen(strLine);	 // 输出所读到的内容
+		
+		count++;
+	}
+	fclose(fp);	 // 关闭读文件
+	printf("\n");
+
